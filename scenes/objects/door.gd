d004a0cfafbe7053e3e7ef742688ac3a9923231d
Elaina Martineau <elainamartineau@gmail.com>
2020-12-24 15:40:22 -0700

    New grass

--- scenes/objects/door.gd
+++ scenes/objects/door.gd
@@ -1,6 +1,23 @@
 extends Spatial
 
+const fade_time = 0.5
+
 export var open = false
+
+onready var sprite_3d = $CharacterPlane/Sprite3D
+
+var fade = 1.0
+var fade_direction = 0
+
+func _process(delta):
+	fade = clamp(fade + fade_direction * delta * 1 / fade_time, 0.0, 1.0)
+	sprite_3d.modulate = Color(fade, fade, fade, 1.0)
+		
+func fade_in():
+	fade_direction = 1
+	
+func fade_out():
+	fade_direction = -1
 
 func interact(i_transform):
 	if open:
